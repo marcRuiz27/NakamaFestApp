ef7627df3beb59f6c8baf33932bd2e8125d6152d
XbraxyDev <ruizmp27@gmail.com>
2024-02-24 11:30:56 +0100

    LoginForm updated and styled

--- screens/LogInForm.jsx
+++ screens/LogInForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {StyleSheet, View, TextInput, Image, Button, Text} from 'react-native';
+import {StyleSheet, View, TextInput, Image, Text, TouchableOpacity} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 
 const LogInS = () => {
@@ -18,11 +18,11 @@ const LogInS = () => {
 
 
     return (
+      <>
+        <View style={styles.containerImage}>
+          <Image source={require('../assets/NakamaFestLogo.jpeg')} style={{ width: 200, height: 200 }}/> 
+        </View>
         <View style={styles.container}>
-          <Image 
-          source={require('../assets/NakamaFestLogo.jpeg')} // Reemplaza 'URL_DE_TU_IMAGEN' con la URL de tu imagen
-          style={styles.image}
-          /> 
           <Controller
             control={control}
             name="user"
@@ -48,11 +48,12 @@ const LogInS = () => {
             name="pass"
             rules={{
               required: true,
+              maxLength: 8,
               //pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
             }}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInput
-                style={styles.input}
+                style={styles.inputPass}
                 onBlur={onBlur}
                 onChangeText={onChange}
                 value={value}
@@ -63,38 +64,53 @@ const LogInS = () => {
             
           />
           {errors.pass && <Text style={styles.errorText}>Please enter a password.</Text>}
-          <Button title="Submit" onPress={handleSubmit(OnSubmit)}/>
-          
+          <TouchableOpacity style={styles.buttonLogin}  onPress={handleSubmit(OnSubmit)}>
+                <Text style={{color:'white'}}>ENTRAR</Text>
+          </TouchableOpacity>
+        
         </View>
+        </>
       );
     };
     
     const styles = StyleSheet.create({
       container: {
-        flex: 1,
+        paddingTop: 0,
         padding: 20,
         justifyContent: 'center', // Centra los elementos en el eje Y (vertical) 
-        //alignItems: 'center', // Centra los elementos en el eje X (horizontal)
       },
-      image: {
-        width: 200, // Establece el ancho de la imagen
-        height: 200, // Establece la altura de la imagen
-        marginBottom: 20, // Espacio debajo de la imagen
-        justifyContent: 'center',
-        alignItems: 'center'
+      containerImage: {
+        paddingTop:150,
+        justifyContent: 'center', // Centra los elementos en el eje Y (vertical) 
+        alignItems: 'center', // Centra los elementos en el eje X (horizontal)
       },
       input: {
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
-        marginBottom: 20,
         padding: 10,
         borderRadius: 5
       },
-      errorText: {
-        padding: 20, // Centra los elementos en el eje Y (vertical) 
-        color: 'red',
+      inputPass:{
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 20,
+        padding: 10,
+        borderRadius: 5
       },
+      buttonLogin:{
+        justifyContent: 'center',
+        alignItems: 'center',        
+        height:40,
+        marginTop:20,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 2,
+        width: 320,
+        backgroundColor: '#2196F3',
+        textAlign: 'center'
+      }
     });
     
     export default LogInS;
